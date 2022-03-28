7558ece233ccccee7757deaeef93ec1dcc4694f2
ungsur <ungsunan@gmail.com>
2022-03-27 20:17:09 -0400

    Working Dag

--- airflow/dags/data_ingestion_gcs_dag.py
+++ airflow/dags/data_ingestion_gcs_dag.py
@@ -13,9 +13,12 @@ from airflow.providers.google.cloud.operators.bigquery import (
 import pyarrow.csv as pv
 import pyarrow.parquet as pq
 import zipfile
+from datetime import datetime
 
 PROJECT_ID = os.environ.get("GCP_PROJECT_ID")
 BUCKET = os.environ.get("GCP_GCS_BUCKET")
+AIRFLOW_HOME = os.environ.get("AIRFLOW_HOME", "/opt/airflow/")
+
 # https://s3.amazonaws.com/tripdata/201501-citibike-tripdata.zip
 dataset_file = "201501-citibike-tripdata.zip"
 dataset_url = f"https://s3.amazonaws.com/tripdata/{dataset_file}"
@@ -24,16 +27,32 @@ csvdataset_file = dataset_file.replace(".zip", ".csv")
 parquet_file = dataset_file.replace(".zip", ".parquet")
 BIGQUERY_DATASET = os.environ.get("BIGQUERY_DATASET", "citibike_data_all")
 
+URL_PREFIX = "https://s3.amazonaws.com/tripdata"
+URL_TEMPLATE = (
+    URL_PREFIX + "/{{ execution_date.strftime('%Y%m') }}-citibike-tripdata.csv.zip"
+)
+OUTPUT_ZIPFILE_TEMPLATE = (
+    AIRFLOW_HOME + "/{{ execution_date.strftime('%Y%m') }}-citibike-tripdata.csv.zip"
+)
+OUTPUT_CSVFILE_TEMPLATE = OUTPUT_ZIPFILE_TEMPLATE.replace(".zip", "")
 
-def format_to_parquet(src_file):
+
+OUTPUT_PQFILE_TEMPLATE = (
+    AIRFLOW_HOME + "/{{ execution_date.strftime('%Y%m') }}-citibike-tripdata.parquet"
+)
+OUTPUT_PQFILE_FILENAME = (
+    "{{ execution_date.strftime('%Y%m') }}-citibike-tripdata.parquet"
+)
+
+
+def format_to_parquet(src_file, csv_file, pq_file):
     if not src_file.endswith(".zip"):
         logging.error("Can only accept source files in ZIP format, for the moment")
         return
     with zipfile.ZipFile(src_file) as z:
         z.extractall()
-
-    table = pv.read_csv(csvdataset_file)
-    pq.write_table(table, csvdataset_file.replace(".csv", ".parquet"))
+    table = pv.read_csv(csv_file)
+    pq.write_table(table, pq_file)
 
 
 # NOTE: takes 20 mins, at an upload speed of 800kbps. Faster if your internet has a better upload speed
@@ -60,32 +79,35 @@ def upload_to_gcs(bucket, object_name, local_file):
 
 default_args = {
     "owner": "airflow",
-    "start_date": days_ago(1),
     "depends_on_past": False,
+    "start_date": datetime(2019, 1, 1),
+    "end_date": datetime(2020, 1, 1),
     "retries": 1,
 }
 
 # NOTE: DAG declaration - using a Context Manager (an implicit way)
 with DAG(
     dag_id="data_ingestion_gcs_dag",
-    schedule_interval="@daily",
     default_args=default_args,
-    catchup=False,
-    max_active_runs=1,
+    catchup=True,
+    schedule_interval="0 6 2 * *",
+    max_active_runs=3,
     tags=["citibike-31437"],
 ) as dag:
 
     download_dataset_task = BashOperator(
         task_id="download_dataset_task",
-        bash_command=f"curl -sSL {dataset_url} > {path_to_local_home}/{dataset_file}",
+        # bash_command='echo "{{ ds }}" "{{ execution_date.strftime(\'%Y%m\') }}"',
+        bash_command=f"curl -sSL {URL_TEMPLATE} > {OUTPUT_ZIPFILE_TEMPLATE}",
     )
 
     format_to_parquet_task = PythonOperator(
         task_id="format_to_parquet_task",
         python_callable=format_to_parquet,
         op_kwargs={
-            "src_file": f"{path_to_local_home}/{dataset_file}",
-            "csv_file": f"{path_to_local_home}/{csvdataset_file}",
+            "src_file": OUTPUT_ZIPFILE_TEMPLATE,
+            "csv_file": OUTPUT_CSVFILE_TEMPLATE,
+            "pq_file": OUTPUT_PQFILE_TEMPLATE,
         },
     )
 
@@ -95,8 +117,8 @@ with DAG(
         python_callable=upload_to_gcs,
         op_kwargs={
             "bucket": BUCKET,
-            "object_name": f"raw/{parquet_file}",
-            "local_file": f"{path_to_local_home}/{parquet_file}",
+            "object_name": f"raw/{OUTPUT_PQFILE_FILENAME}",
+            "local_file": f"{OUTPUT_PQFILE_TEMPLATE}",
         },
     )
 
@@ -110,7 +132,7 @@ with DAG(
             },
             "externalDataConfiguration": {
                 "sourceFormat": "PARQUET",
-                "sourceUris": [f"gs://{BUCKET}/raw/{parquet_file}"],
+                "sourceUris": [f"gs://{BUCKET}/raw/{OUTPUT_PQFILE_FILENAME}"],
             },
         },
     )
